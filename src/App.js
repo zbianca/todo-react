5b860f40ab1e42769674970399d396ccb5b87a50
zbianca <bianca.zanardi@gmail.com>
2019-04-01 23:57:23 +0200

    add and display todo-item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import uuid from 'uuid';
 
 class App extends Component {
+
+  state = {
+    query: '',
+    tasks: [],
+  };
+
+  updateQuery = query => {
+    this.setState({ query });
+  };
+
+  addTask = e => {
+    e.preventDefault();
+    let tasks = [...this.state.tasks];
+    let newTask = { name: this.state.query, id: uuid.v4() }
+    tasks.unshift(newTask);
+    this.setState({ tasks, query: '' });
+  };
+
+  deleteTask = (id) => {
+    let tasks = this.state.tasks.filter(task => task.id !== id);
+    this.setState({ tasks });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+        <div className="todo">
+          <form onSubmit={event => this.addTask(event)}>
+            <label htmlFor="new-task" className="hide">New task</label>
+            <div>
+              <input type="text"
+                id="new-task"
+                name="new-task"
+                className="input"
+                placeholder="New task"
+                value={this.state.query}
+                onChange={event => this.updateQuery(event.target.value)}
+              ></input>
+              <button type="submit">→</button>
+            </div>
+          </form>
+          {this.state.tasks.length > 0 ? (
+            <ul>
+              {this.state.tasks.map(item => (
+                <li key={item.id} className="todo-item">{item.name} <button onClick={() => this.deleteTask(item.id)}>x</button></li>
+              ))}
+            </ul>
+          ) : <p>No tasks</p>}
+        </div>
       </div>
     );
   }
